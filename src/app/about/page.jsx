796d47f50941173d36d327053ddeee0a1ef41eaa
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-07-08 12:23:54 +0600

    learn: image tag

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 
 const page = () => {
     return (
         <div>
             <h1>this is about page</h1>
+           <Image src="/next.svg" alt="next.js logo image" width="300" height="300"></Image>
         </div>
     );
 };
